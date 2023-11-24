a6d6be11b3db8a032dc575cba066c489a1db5ab4
Amin Khorasani <amin.a.khorasani@gmail.com>
2023-11-24 20:27:59 +0330

    Ad and Advertiser model created

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Advertiser(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=400)
+    link = models.CharField
+    img_url = models.CharField()
+    clicks = models.PositiveIntegerField()
+    views = models.PositiveIntegerField()
+    
