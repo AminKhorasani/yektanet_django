cee4c248216129730840ad7f3990558ac79691c3
Amin Khorasani <amin.a.khorasani@gmail.com>
2023-11-25 21:01:05 +0330

    view was class-based

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -8,7 +8,7 @@ class Advertiser(models.Model):
 
 
 class Ad(models.Model):
-    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE, related_name='ads',null=True)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE, related_name='ads', null=True)
     title = models.CharField(max_length=400)
     link = models.CharField(max_length=400)
     img_url = models.CharField(max_length=400)
@@ -16,11 +16,11 @@ class Ad(models.Model):
 
 class View(models.Model):
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE, null=True)
-    ip = models.GenericIPAddressField()
+    ip = models.CharField(max_length=100)
     view_time = models.DateTimeField()
 
 
 class Click(models.Model):
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE, null=True)
-    ip = models.GenericIPAddressField()
+    ip = models.CharField(max_length=100)
     click_time = models.DateTimeField()
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
-from advertiser_management.models import Advertiser, Ad, Click, View
-from django.shortcuts import redirect
+from advertiser_management.models import Advertiser, Ad, Click, View as ViewModel
+from django.shortcuts import redirect, get_object_or_404
 from .forms import AdForm
 from django.urls import reverse
 from datetime import datetime
+from django.views.generic import View, TemplateView, RedirectView
 
 
-def index(request):
-    advertisers = Advertiser.objects.all()
-    context = {'advertisers': advertisers}
+class HomeView(TemplateView):
+    template_name = 'advertiser_management/ads.html'
 
-    for advertiser in advertisers:
-        ads_list = advertiser.ads.all()
-        for ad in ads_list:
-            View.objects.create(ad=ad, ip='', view_time=datetime.now())
+    def get_context_data(self, **kwargs):
+        advertisers = Advertiser.objects.all()
 
-    return render(request, 'advertiser_management/ads.html', context)
-
-
-def ad_inc_clicks(request, object_id):
-    ad = Ad.objects.get(id=object_id)
-    Click.objects.create(ad=ad, ip='', click_time=datetime.now())
-    return redirect(ad.link)
+        for advertiser in advertisers:
+            for ad in advertiser.ads.all():
+                ViewModel.objects.create(ad=ad, ip='', view_time=datetime.now())
+        context = {'advertisers': advertisers}
+        return context
 
 
-def ad_creator(request):
-    form = AdForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    if request.method == 'GET':
+class AdIncClicksView(RedirectView):
+
+    permanent = False
+    query_string = True
+    pattern_name = 'ad-detail'
+
+    def get_redirect_url(self, *args, **kwargs):
+        ad = get_object_or_404(Ad, id=kwargs['object_id'])
+        return ad.link
+
+
+class AdCreatorView(TemplateView):
+    template_name = 'advertiser_management/ad_create.html'
+
+    def get_context_data(self, **kwargs):
+        form = AdForm()
         context = {'form': form}
-        return render(request,'advertiser_management/ad_create.html', context)
-    else:
+        return context
+
+    @staticmethod
+    def post(request):
+        form = AdForm(request.POST)
+        if form.is_valid():
+            form.save()
         return redirect(reverse('advertiser_list'))
